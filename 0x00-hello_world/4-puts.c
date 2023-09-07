bacfc517f12c907abf7850776db597abfe497575
CollinsAnyona <collinsnayona66@gmail.com>
2023-09-07 08:43:06 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main . A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	/**
-	* use the puts function to print the message
-	*/
-	puts("\'Programming is like building a multilingual puzzle");
-	/**
-	* End the program with the value 0
-	*/
-	return (0);
+puts("Programming is like building a multilingual puzzle");
+
+return (0);
 }
