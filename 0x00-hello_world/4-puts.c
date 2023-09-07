1115f97b75f885e3bd8fba8b04542d3507b6a13e
CollinsAnyona <collinsnayona66@gmail.com>
2023-09-07 11:32:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main . A program that prints a line with puts function
+ * main - A program that prints a line with puts function
+ * A puzzle followed by a new line
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: [Your Name]
+ * Desc: A C program that demonstrates the use of printf.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
 #include <stdio.h>
 /**
- * main . A program that prints a ine using printf function
+ * main: A program that prints a ine using printf function
  * Return: 0 (Success)
  */
 int main(void)
