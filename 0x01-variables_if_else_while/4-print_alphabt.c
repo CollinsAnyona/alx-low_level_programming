49a8ff03106a4db4cf43ef859edaaccaa8801f86
CollinsAnyona <collinsnayona66@gmail.com>
2023-09-11 09:04:41 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints a value
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch != 'q' && ch != 'e')
+		{
+		putchar(ch);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
