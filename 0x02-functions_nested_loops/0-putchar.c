9a44dfba4a9a6623beb8a3dcb9345facbf057ed6
CollinsAnyona <collinsnayona66@gmail.com>
2023-09-12 11:57:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints an output _putchar
+ * Return: always 0
+ */
+int main(void)
+{
+	/* the output */
+	printf("_putchar\n");
+
+	return (0);
+}
+
